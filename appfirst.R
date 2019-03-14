58b5b94bf27666d184e451a8b04573db420954cb
victor <100316899@alumnos.uc3m.es>
2019-03-14 19:35:30 +0100

    I have included tabs. Put what you have done in a new tab.

--- appfirst.R
+++ appfirst.R
@@ -13,10 +13,12 @@ library(factoextra)
 
 source("data_tidying_inc.R")
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- navbarPage("Shiny App",
+                 tabPanel("Cluster Analysis",
+                 fluidPage(
    
-   # Application title
-   titlePanel("Cluster Plot"),
+
+   
    selectInput("Round", label = h5("Select Round"), 
                choices = c(5:26), 
                selected = 7),
@@ -26,6 +28,23 @@ ui <- fluidPage(
    
   
    plotOutput(outputId = "plot_clusters")
+                 )
+                 ),
+   tabPanel("Evolution of the points",
+            fluidPage(
+              selectInput("Round2", label=h5("Select Round"),
+                          choices=c(1:26), 
+                          selected=5),
+              
+              selectInput("variable", label=h5("Select Variable"),
+                          choices=colnames(matrix), selected=colnames(matrix)[5]),
+              
+              plotOutput(outputId = "Points_bar")
+              
+            )
+            ),
+
+   
 )
 
    server=function(input,output){
@@ -35,6 +54,16 @@ ui <- fluidPage(
      rownames(b)=unique(matrix$Team)
      fviz_cluster(a, b)
    })
+   output$Points_bar<-renderPlot({
+     a=matrix[matrix$Round==input$Round2,]
+     ggplot(a)+aes_string(x="Team", y=input$variable, fill="Team")+geom_bar(stat="identity")
+       
+   })
+   
+   
+   
+   
+   
    
    }
    
