e2f1cfaf56fd6bbe4d169376c9d8d8593b89779d
cynthiamdrs <47811597+cynthiamdrs@users.noreply.github.com>
2019-03-29 11:36:47 +0100

    Now heatmap works!

--- Data_Tidying.R
+++ Data_Tidying.R
@@ -39,7 +39,18 @@ ui <- navbarPage("Shiny App",
                                         selected = unique(matrix$Team)[5]),
                             plotOutput(outputId = "plot_shots")
                            )
-                 )
+                 ),
+                 tabPanel("Shots to Goal Heatmap",
+                          fluidPage(
+                            mainPanel( 
+                              #this will create a space for us to display our map
+                              leafletOutput(outputId = "mymap"), 
+                              #this allows me to put the checkmarks ontop of the map to allow people to view STG depth or overlay a heatmap
+                              absolutePanel(top = 60, left = 20, 
+                                            checkboxInput("markers", "Depth", FALSE),
+                                            checkboxInput("heat", "Heatmap", FALSE)
+                              )
+                            )))
 )
 
 server=function(input,output){
@@ -60,8 +71,56 @@ server=function(input,output){
       ggplot(hn)+aes_string(x="Round", y=input$Stat)+
         geom_line(aes(group=hn$Team, colour=hn$Team))
     })
+    #define the color pallate for the 
+    pal <- colorNumeric(
+      palette = c('gold','orange red', 'dark red'),
+      domain = data$STG,
+      na.color = "lightgray")
+    
+    #define the color of for the depth of the earquakes
+    pal2 <- colorFactor(
+      palette = c('blue', 'yellow', 'red'),
+      domain = data$depth_type,
+      na.color="lightgray"
+    )
+    
+    #create the map
+    output$mymap <- renderLeaflet({
+      leaflet(data) %>% 
+        setView(lng = -2, lat = 40, zoom = 4)  %>% #setting the view over ~ center of Europe
+        addTiles() %>% 
+        addCircles(data = data, lat = ~ latitude, lng = ~ longitude, weight = 1, radius = ~sqrt(STG)*25000, popup = ~as.character(STG), label = ~as.character(paste0("Magnitude: ", sep = " ", STG)), color = ~pal(STG), fillOpacity = 0.5)
+    })
+    
+    #next we use the observe function to make the checkboxes dynamic. If you leave this part out you will see that the checkboxes, when clicked on the first time, display our filters...But if you then uncheck them they stay on. So we need to tell the server to update the map when the checkboxes are unchecked.
+    observe({
+      proxy <- leafletProxy("mymap", data = data)
+      proxy %>% clearMarkers()
+      if (input$markers) {
+        proxy %>% addCircleMarkers(stroke = FALSE, color = ~pal2(depth_type), fillOpacity = 0.2,      label = ~as.character(paste0("Magnitude: ", sep = " ", STG))) %>%
+          addLegend("bottomright", pal = pal2, values = data$depth_type,
+                    title = "Depth Type",
+                    opacity = 1)}
+      else {
+        proxy %>% clearMarkers() %>% clearControls()
+      }
+    })
+    
+    observe({
+      proxy <- leafletProxy("mymap", data = data)
+      proxy %>% clearMarkers()
+      if (input$heat) {
+        proxy %>%  addHeatmap(lng=~longitude, lat=~latitude, intensity = ~STG, blur =  10, max = 0.05, radius = 15) 
+      }
+      else{
+        proxy %>% clearHeatmap()
+      }
+      
+      
+    })
 }
 
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
